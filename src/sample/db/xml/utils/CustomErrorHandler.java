7b5cffd67f90f9934110aa89f0f105eccda10e44
rgarciacarmona <r.garcia.carmona@gmail.com>
2018-04-26 16:26:59 +0200

    Removed unused imports.

--- src/sample/db/xml/utils/CustomErrorHandler.java
+++ src/sample/db/xml/utils/CustomErrorHandler.java
@@ -1,7 +1,5 @@
 package sample.db.xml.utils;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
