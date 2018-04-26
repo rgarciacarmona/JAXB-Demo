415b1e4c11d02895165991b99913e0b44f3c07c7
rgarciacarmona <r.garcia.carmona@gmail.com>
2018-04-26 16:24:12 +0200

    Comments added.

--- src/sample/db/xml/DTDCheckerReport.java
+++ src/sample/db/xml/DTDCheckerReport.java
@@ -15,11 +15,15 @@ public class DTDCheckerReport {
     public static void main(String[] args) {
         File xmlFile = new File("./xmls/External-Report.xml"); 
         try {
+        	// Create a DocumentBuilderFactory
             DocumentBuilderFactory dBF = DocumentBuilderFactory.newInstance();
+            // Set it up so it validates XML documents
             dBF.setValidating(true);
+            // Create a DocumentBuilder and an ErrorHandler (to check validity)
             DocumentBuilder builder = dBF.newDocumentBuilder();
             CustomErrorHandler customErrorHandler = new CustomErrorHandler();
             builder.setErrorHandler(customErrorHandler);
+            // Parse the XML file and print out the result
             Document doc = builder.parse(xmlFile);
             if (customErrorHandler.isValid()) {
                 System.out.println(xmlFile + " was valid!");
